9c518c27e7de73e44bcb6cb3c8ece239a3da055c
Joseph Guhlin <joseph.guhlin@gmail.com>
2022-11-30 09:43:52 +1300

    .

---
+++ weights_calculation/pub/run_multiple.sh
@@ -0,0 +1,27 @@
+#python calculate_kmer_encoder_weights_nucleotide.py 21 8
+#python calculate_kmer_encoder_weights_nucleotide.py 21 16
+#python calculate_kmer_encoder_weights_nucleotide.py 21 32
+#python calculate_kmer_encoder_weights_nucleotide.py 21 64
+#python calculate_kmer_encoder_weights_nucleotide.py 21 128
+#python calculate_kmer_encoder_weights_nucleotide.py 21 256
+
+#python calculate_kmer_encoder_weights_nucleotide.py 11 8
+#python calculate_kmer_encoder_weights_nucleotide.py 11 16
+#python calculate_kmer_encoder_weights_nucleotide.py 11 32
+#python calculate_kmer_encoder_weights_nucleotide.py 11 64
+#python calculate_kmer_encoder_weights_nucleotide.py 11 128
+#python calculate_kmer_encoder_weights_nucleotide.py 11 256
+
+#python calculate_kmer_encoder_weights_amino.py 9 8
+#python calculate_kmer_encoder_weights_amino.py 9 16
+#python calculate_kmer_encoder_weights_amino.py 9 32
+python calculate_kmer_encoder_weights_amino.py 9 64
+python calculate_kmer_encoder_weights_amino.py 9 128
+python calculate_kmer_encoder_weights_amino.py 9 256
+
+#python calculate_kmer_encoder_weights_amino.py 21 8
+#python calculate_kmer_encoder_weights_amino.py 21 16
+#python calculate_kmer_encoder_weights_amino.py 21 32
+python calculate_kmer_encoder_weights_amino.py 21 64
+python calculate_kmer_encoder_weights_amino.py 21 128
+python calculate_kmer_encoder_weights_amino.py 21 256
